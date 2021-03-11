2798b72bdd898f693909b7691b73a5560193452d
kole1337 <nikola.parushev2003@gmail.com>
2021-03-11 13:25:32 +0000

    password shower
    showing the entered password

---
+++ password/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.displayPasswordButton = new System.Windows.Forms.Button();
+            this.displayPasswordLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputPasswordTextBox
+            // 
+            this.inputPasswordTextBox.Location = new System.Drawing.Point(12, 12);
+            this.inputPasswordTextBox.Name = "inputPasswordTextBox";
+            this.inputPasswordTextBox.Size = new System.Drawing.Size(237, 23);
+            this.inputPasswordTextBox.TabIndex = 2;
+            this.inputPasswordTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // displayPasswordButton
+            // 
+            this.displayPasswordButton.Location = new System.Drawing.Point(94, 77);
+            this.displayPasswordButton.Name = "displayPasswordButton";
+            this.displayPasswordButton.Size = new System.Drawing.Size(68, 23);
+            this.displayPasswordButton.TabIndex = 3;
+            this.displayPasswordButton.Text = "Show me";
+            this.displayPasswordButton.UseVisualStyleBackColor = true;
+            this.displayPasswordButton.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // displayPasswordLabel
+            // 
+            this.displayPasswordLabel.AutoSize = true;
+            this.displayPasswordLabel.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.displayPasswordLabel.Location = new System.Drawing.Point(12, 51);
+            this.displayPasswordLabel.Name = "displayPasswordLabel";
+            this.displayPasswordLabel.Size = new System.Drawing.Size(13, 20);
+            this.displayPasswordLabel.TabIndex = 6;
+            this.displayPasswordLabel.Text = " ";
+            this.displayPasswordLabel.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 130);
+            this.Controls.Add(this.displayPasswordLabel);
+            this.Controls.Add(this.displayPasswordButton);
+            this.Controls.Add(this.inputPasswordTextBox);
+            this.Name = "Form1";
+            this.Text = "Simple Event Example";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputPasswordTextBox;
+        private System.Windows.Forms.Button displayPasswordButton;
+        private System.Windows.Forms.Label displayPasswordLabel;
+    }
+}
+
